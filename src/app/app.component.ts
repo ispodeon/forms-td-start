35b94df8cec1ccd90074bc507f103c9329d16069
Sandaime <ispodeon@yahoo.com>
2021-05-02 17:20:34 -0400

    added many changes and additions to the form using form group controls and using the form class in typescript to extract data for display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,35 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm; // you can use viewchild to access the NgForm as well and without submitting beforehand
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: HTMLFormElement) {
@@ -19,6 +45,14 @@ export class AppComponent {
   // }
   onSubmit(){
     console.log(this.signupForm);
+
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    
+    this.submitted = true;
   }
 
 }
